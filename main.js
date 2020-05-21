534182446ebdb5f57eaa6c49c421b00769b89fb8
msadaike <msadaike@users.noreply.github.com>
2020-05-21 09:11:15 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
-
-var mySlider = $("#EX1").bootstrapSlider({
-  ticks: [0, 100, 200, 300, 400],
-  ticks_labels: ['', 'This', '', 'That', ''],
-  
-});
